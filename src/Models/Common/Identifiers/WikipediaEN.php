3e93724288dcfd7b988fae6bed3877b80d72bd66
Irfan <irfan-dahir@users.noreply.github.com>
2023-10-19 22:36:52 +0500

    update identifiers

--- src/Models/Common/Identifiers/WikipediaEN.php
+++ src/Models/Common/Identifiers/WikipediaEN.php
@@ -4,7 +4,7 @@ namespace Hiyori\Models\Common\Identifiers;
 
 class WikipediaEN extends Identifier
 {
-    public const SHORTHAND = "wp_en";
+    public const SHORTHAND = "wpen";
     public const SOURCE_NAME = "WikipediaEN";
     public const PATTERN = "en\.wikipedia\.org\/wiki\/(.*)";
     public const ENTRY_URL = "https://en.wikipedia.org/wiki/%s";
--- src/Models/Common/Identifiers/WikipediaJA.php
+++ src/Models/Common/Identifiers/WikipediaJA.php
@@ -4,7 +4,7 @@ namespace Hiyori\Models\Common\Identifiers;
 
 class WikipediaJA extends Identifier
 {
-    public const SHORTHAND = "wp_ja";
+    public const SHORTHAND = "wpja";
     public const SOURCE_NAME = "WikipediaJA";
     public const PATTERN = "ja\.wikipedia\.org\/wiki\/(.*)";
     public const ENTRY_URL = "https://ja.wikipedia.org/wiki/%s";
